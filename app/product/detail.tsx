961fef5bc9b1416baffe58d140a37ca328e7e05a
kamharida1 <kamharida007@gmail.com>
2023-03-27 04:54:14 +0100

    Expo Router Initialized

---
+++ app/product/detail.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "@bacons/react-views";
+
+export default function Detail() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text> Detail Screen </Text>
+    </View>
+  );
+}
